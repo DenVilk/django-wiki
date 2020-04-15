79d040782fbeb8cc3989627a53a6864ba789547e
Velikovich Vladimir <denvilk@raspberrypi>
2020-04-15 19:47:38 +0300

    Setted settings

--- install.sh
+++ install.sh
@@ -10,10 +10,10 @@ source env/bin/activate
 pip install -U pip
 pip install -r requirements.txt
 
-sed -i "s~dbms_template_path~$project_path~g" systemd/gunicorn.service
+sed -i "s~dbms_template_path~$project_path~g" systemd/wiki.service
 sed -i "s~dbms_template_domain~$project_domain~g" src/config/settings.py
 
-sudo ln -s $project_path/systemd/gunicorn.service /etc/systemd/system/
+sudo ln -s $project_path/systemd/wiki.service /etc/systemd/system/
 
 sudo systemctl daemon-reload
 sudo systemctl start gunicorn
