e759218910e770d8575f1481e33ab7843809ed0c
denvilk <vvelikovich@gmail.com>
2020-04-15 19:26:11 +0300

    Initial commit

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+base_python_interpreter=""
+project_domain=""
+project_path=`pwd`
+
+read -p "Python interpreter: " base_python_interpreter
+read -p "Your domain without protocol (for example, google.com): " project_domain
+`$base_python_interpreter -m venv env`
+source env/bin/activate
+pip install -U pip
+pip install -r requirements.txt
+
+sed -i "s~dbms_template_path~$project_path~g" systemd/gunicorn.service
+sed -i "s~dbms_template_domain~$project_domain~g" src/config/settings.py
+
+sudo ln -s $project_path/systemd/gunicorn.service /etc/systemd/system/
+
+sudo systemctl daemon-reload
+sudo systemctl start gunicorn
+sudo systemctl enable gunicorn
